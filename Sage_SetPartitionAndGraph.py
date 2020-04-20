4596091cba5dc6207e92a35809af242fb1d358d5
yiruiliu <yirui_liu@yahoo.com>
2020-04-20 13:57:09 -0400

    Add files via upload

---
+++ Sage_SetPartitionAndGraph.py
@@ -0,0 +1,25 @@
+def subfunc1(Mat,P1,P2,P12,l1,l2):
+	for s in P12:
+		for i in range(0, l1):
+			if s.issubset(P1[i]):
+				break
+		for j in range(0, l2):
+			if s.issubset(P2[j]):
+				break
+		Mat=Matrix(Mat)
+		Mat[i,j] = 1
+	return Mat
+
+def FromSetPartitions2Matrix(P1,P2):
+    P12 = P1 * P2
+    l1 = len(P1)
+    l2 = len(P2)
+    row = [0] * l1
+    Mat = [row] * l2
+    Mat = subfunc1(Mat, P1, P2, P12, l1, l2)
+    return Mat
+
+def FromSetPartitons2BipartiteGraph(P1,P2):
+	Mat = FromSetPartitions2Matrix(P1,P2)
+	G = BipartiteGraph(Mat.T)
+	return G
