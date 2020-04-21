972a7a6a3c9292dd0123c2ca94a505be66df1997
yiruiliu <yirui_liu@yahoo.com>
2020-04-21 00:31:33 -0400

    Update Sage_SetPartitionAndGraph.py

--- Sage_SetPartitionAndGraph.py
+++ Sage_SetPartitionAndGraph.py
@@ -19,7 +19,30 @@ def FromSetPartitions2Matrix(P1,P2):
     Mat = subfunc1(Mat, P1, P2, P12, l1, l2)
     return Mat
 
-def FromSetPartitons2BipartiteGraph(P1,P2):
-	Mat = FromSetPartitions2Matrix(P1,P2)
-	G = BipartiteGraph(Mat.T)
-	return G
+def FromSetPartitons2BipartiteGraph(P1,P2,baseCar,CarOfP12):
+	P12 = P1 * P2
+	if P12.max_block_size() == baseCar/CarOfP12 and len(P12) == CarOfP12:
+		Mat = FromSetPartitions2Matrix(P1,P2)
+		G = BipartiteGraph(Mat.T)
+		return G
+	return []
+
+def CanonicalGraphList(P1,Partitions,CarofP12):
+	baseCar = P1.base_set_cardinality()
+	CanonicalGlist = []
+	for P2 in Partitions:
+		G = FromSetPartitons2BipartiteGraph(P1,P2,baseCar,CarOfP12)
+		if G:
+			if not CanonicalGlist:
+				CanonicalGlist.append(G)
+			else:
+				verify = 0
+				for CanonicalG in CanonicalGlist:
+					if CanonicalG.is_isomorphic(G):
+						verify = 1
+						break
+				if verify == 0:
+					CanonicalGlist.append(G)
+	return CanonicalGlist
+
+
